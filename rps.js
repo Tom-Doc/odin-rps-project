a4d0e9aeaa09727af39e8f1ac8499a3aae004abc
Tom <Tomdocherty1@gmail.com>
2022-06-09 19:00:37 -0700

    Everything is done and everything is working correctly. I fixed my playRound function so that it tallys the score's correctly, and fixed the if/else in game() so that it displays a winner.

--- rps.js
+++ rps.js
@@ -1,28 +1,15 @@
 "use strict";
 
-// const calcTip = function (bill) {
-//     return bill <= 300 && bill >=50 ? bill * 0.15 : bill * 0.2;
-// }
-    
-// for (let i = 0; i < bills.length; i++) {   
-//     const tip = calcTip(bills[i]);
-//     tips.push(tip);
-//     const total = bills[i]+tips[i]
-//     totals.push(total);     
-// }
+let playerScore = 0;
+let computerScore = 0;
+let tie = 0;
 
 
 
-const playerChoice = function() {
-    const playerSelection = prompt("Please choose Rock, Paper, or Scissors").toLowerCase();
-    return playerSelection
-}
-
-
-
+//This is our function that allows the computer to pick rock, paper, or scissors.
 const computerChoice = function () {
     let compSelection = Math.random();
-    if (compSelection <= 0.34) {
+    if (compSelection < 0.34) {
         compSelection = "rock";
     } else if (compSelection <= 0.67) {
         compSelection = "paper";
@@ -32,86 +19,68 @@ const computerChoice = function () {
     return compSelection;
 }
 
-let playerScore = 0;
-let computerScore = 0;
-let tie = 0;
-let winnerOfGame = "";
-
-
-const playRound = function (playerSelection, computerSelection) {
+//This is our function that goes through all the logic of rock, paper, scissors
+//and can determine who wins each round depending on what they picked. 
+const playRound = function(playerSelection, computerSelection) {
     //rock Scenarios
-    if (playerSelection == "rock" && computerSelection == "scissors") {
-        playerScore +=1;
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore++;
         return "You Win! - Rock beats Scissors";
         
-    } else if (playerSelection == "rock" && computerSelection == "paper") {
-        computerScore +=1;
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        computerScore++;
         return "Computer Wins! - Paper beats Rock";
-    } else if (playerSelection == "rock" && computerSelection == "rock") {
-        tie +=1;
+    } else if (playerSelection === "rock" && computerSelection === "rock") {
+        tie++;
         return "Draw! You both selected Rock."
     }
     // scissors Scenarios
-    else if (playerSelection == "scissors" && computerSelection == "paper") {
-        playerScore +=1;
+    else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore++;
         return "You Win! - Scissors beats Paper";
-    } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        computerScore +=1;
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        computerScore++;
         return "Computer Wins! - Rock beats Scissors";
-    } else if (playerSelection == "scissors" && computerSelection == "scissors") {
-        tie +=1;
+    } else if (playerSelection === "scissors" && computerSelection === "scissors") {
+        tie++;
         return "Draw! You both selected Scissors";
     }
     //paper Scenarios
-    else if (playerSelection == "paper" && computerSelection == "rock") {
-        playerScore +=1;
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore++;
         return "You Win! - Paper beats Rock";
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        computerScore +=1;
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        computerScore++;
         return "Computer wins! - Scissors beats Paper";
-    } else if (playerSelection == "paper" && computerSelection == "paper") {
-        tie +=1;
+    } else if (playerSelection === "paper" && computerSelection === "paper") {
+        tie++;
         return "Draw! You both selected Paper";
     } else {
         return "Uh oh! Something went wrong. Please try again.";
     }
+    
 }
 
+//This is out game function that will loop through the playRounds function and it
+//will have us play 5 rounds. At the end of the 5 rounds the if/else statement
+//then lets us know who won out of the 5 rounds. 
 const game = function() {
     for (let i = 0; i < 5; i++) {
-        const playerSelection = playerChoice();
+        const playerSelection = prompt("Please choose Rock, Paper, or Scissors").toLowerCase();
         const computerSelection = computerChoice();
+        // playRound(playerSelection, computerSelection);
         console.log(`You selected ${playerSelection}`);
         console.log(`The computer selected ${computerSelection}`);
-        console.log(playRound(playerSelection, computerSelection));
+        console.log(playRound(playerSelection, computerSelection));  //<-- Note to future self. Your score was being doubled because you had your playRound function here as well as above. When you remove the console.log everything will still work you just wont see the ""
         console.log(playerScore, computerScore, tie);
-       
-    }
+        }
+        if (playerScore > computerScore && playerScore >= tie) {
+            console.log("You beat the computer!");
+        } else if (computerScore > playerScore && computerScore >= tie) {
+            console.log("Computer wins. Try again!");
+        } else {
+            console.log("The game has ended in a tie.");  
+        }
 }
 
-
-// if (playerScore >= 3) {
-//     console.log(`You win!`);
-// } else  if (computerScore >= 3) {
-//     console.log(`Computer wins!`);
-// }
-// }
-// const playerSelection = playerChoice();
-// const computerSelection = computerChoice();
-// console.log(`You selected ${playerSelection}`);
-// console.log(`The computer selected ${computerSelection}`);
-
-// console.log(playRound(playerSelection, computerSelection));
-
 game();
-
-
-// These were for testing purposes
-// const playerSelection = playerChoice();
-// const computerSelection = computerChoice();
-// console.log(playRound(playerSelection, computerSelection));
-
-
-
-
-
